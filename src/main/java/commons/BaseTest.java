8453f4a28c5af6411f0a4260adbd19351cc0be7e
Nguyễn Ngọc Sơn <nnson0310@gmail.com>
2022-06-22 22:03:14 +0700

    Create project structure

---
+++ src/main/java/commons/BaseTest.java
@@ -0,0 +1,206 @@
+package commons;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.File;
+import java.util.Random;
+
+public abstract class BaseTest {
+    private WebDriver driver;
+
+    protected final Log log;
+
+    private String projectPath = System.getProperty("user.dir");
+
+    public BaseTest() {
+        log = LogFactory.getLog(getClass());
+    }
+
+    public int generateRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(9999);
+    }
+
+    public static void sleepInSeconds(long seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Custom Assert
+    protected boolean verifyTrue(boolean condition) {
+        boolean pass = true;
+        System.out.println("Verification Start");
+        try {
+            System.out.println(" -------------------------- PASSED -------------------------- ");
+            Assert.assertTrue(condition);
+        } catch (Throwable e) {
+            System.out.println(" -------------------------- FAILED -------------------------- ");
+            pass = false;
+
+            // Add lỗi vào ReportNG
+            VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+        }
+        return pass;
+    }
+
+    protected boolean verifyFalse(boolean condition) {
+        boolean pass = true;
+        System.out.println("Verification Start");
+        try {
+            System.out.println(" -------------------------- PASSED -------------------------- ");
+            Assert.assertFalse(condition);
+        } catch (Throwable e) {
+            System.out.println(" -------------------------- FAILED -------------------------- ");
+            pass = false;
+            VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+        }
+        return pass;
+    }
+
+    protected boolean verifyEquals(Object actual, Object expected) {
+        boolean pass = true;
+        System.out.println("Verification Start");
+        try {
+            Assert.assertEquals(actual, expected);
+            System.out.println(" -------------------------- PASSED -------------------------- ");
+        } catch (Throwable e) {
+            pass = false;
+            System.out.println(" -------------------------- FAILED -------------------------- ");
+            VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+        }
+        return pass;
+    }
+
+    @BeforeSuite
+    public void deleteAllureReport() {
+        deleteAllFileInFolder("/allure-json");
+    }
+
+    public void deleteAllFileInFolder(String reportName) {
+        try {
+            String pathFolderDownload = GlobalConstants.getGlobalConstants().getProjectPath() + reportName;
+            File file = new File(pathFolderDownload);
+            File[] listOfFiles = file.listFiles();
+            for (int i = 0; i < listOfFiles.length; i++) {
+                if (listOfFiles[i].isFile()) {
+                    new File(listOfFiles[i].toString()).delete();
+                }
+            }
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+
+
+    protected String getCurrentDay() {
+        DateTime nowUTC = new DateTime(DateTimeZone.UTC);
+        int day = nowUTC.getDayOfMonth();
+        if (day < 10) {
+            String dayValue = "0" + day;
+            return dayValue;
+        }
+        return String.valueOf(day);
+    }
+
+    protected String getCurrentMonth() {
+        DateTime now = new DateTime(DateTimeZone.UTC);
+        int month = now.getMonthOfYear();
+        if (month < 10) {
+            String monthValue = "0" + month;
+            return monthValue;
+        }
+        return String.valueOf(month);
+    }
+
+    protected String getCurrentYear() {
+        DateTime now = new DateTime(DateTimeZone.UTC);
+        return String.valueOf(now.getYear());
+    }
+
+    /**
+     * Get today with format
+     * @return today
+     */
+    protected String getToday() {
+        String today = getCurrentMonth() + "/" + getCurrentDay() + "/" + getCurrentYear();
+        System.out.println(today);
+        return today;
+    }
+
+    public WebDriver getDriver() {
+        return this.driver;
+    }
+
+    public void closeBrowserAndKillProcess() {
+        String cmd = "";
+        try {
+            String osName = System.getProperty("os.name").toLowerCase();
+            log.info("OS name = " + osName);
+
+            String driverInstanceName = driver.toString().toLowerCase();
+            log.info("Driver instance name = " + driverInstanceName);
+
+            if (driverInstanceName.contains("chrome")) {
+                if (osName.contains("window")) {
+                    cmd = "taskkill /F /FI \"IMAGENAME eq chromedriver*\"";
+                } else {
+                    cmd = "pkill chromedriver";
+                }
+            } else if (driverInstanceName.contains("internetexplorer")) {
+                if (osName.contains("window")) {
+                    cmd = "taskkill /F /FI \"IMAGENAME eq IEDriverServer*\"";
+                }
+            } else if (driverInstanceName.contains("firefox")) {
+                if (osName.contains("windows")) {
+                    cmd = "taskkill /F /FI \"IMAGENAME eq geckodriver*\"";
+                } else {
+                    cmd = "pkill geckodriver";
+                }
+            } else if (driverInstanceName.contains("edge")) {
+                if (osName.contains("window")) {
+                    cmd = "taskkill /F /FI \"IMAGENAME eq msedgedriver*\"";
+                } else {
+                    cmd = "pkill msedgedriver";
+                }
+            } else if (driverInstanceName.contains("opera")) {
+                if (osName.contains("window")) {
+                    cmd = "taskkill /F /FI \"IMAGENAME eq operadriver*\"";
+                } else {
+                    cmd = "pkill operadriver";
+                }
+            } else if (driverInstanceName.contains("safari")) {
+                if (osName.contains("mac")) {
+                    cmd = "pkill safaridriver";
+                }
+            }
+
+            if (driver != null) {
+                driver.manage().deleteAllCookies();
+                driver.quit();
+            }
+        } catch (Exception e) {
+            log.info(e.getMessage());
+        } finally {
+            try {
+                Process process = Runtime.getRuntime().exec(cmd);
+                process.waitFor();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
